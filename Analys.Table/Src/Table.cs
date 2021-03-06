35b63bb06694de8c5570770c2539df74d26d007e
hoyeungw <hoyeungw@outlook.com>
2021-03-06 09:32:27 +0800

    add setter for Table index

--- Analys.Table/Src/Table.cs
+++ Analys.Table/Src/Table.cs
@@ -25,7 +25,11 @@ namespace Analys {
     public int Width => this.Rows.Width();
 
     public T[] this[int x] => this.Rows.Row(x);
-    public T this[int x, string h] => this.Rows[x, this.CoIn(h)];
+
+    public T this[int x, string h] {
+      get => this.Rows[x, this.CoIn(h)];
+      set => this.Rows[x, this.CoIn(h)] = value;
+    }
 
     public T[] Column(string column) => this.Rows.Column(this.CoIn(column));
 
