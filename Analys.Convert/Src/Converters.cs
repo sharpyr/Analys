76efd07ea22f2414744a1ef71d058cb31403f38a
hoyeungw <hoyeungw@outlook.com>
2021-08-16 11:12:16 +0800

    add MutableTableToTable

---
+++ Analys.Convert/Src/Converters.cs
@@ -0,0 +1,15 @@
+﻿using Veho.List;
+using Veho.NestedVector;
+
+namespace Analys {
+  public static class Converters {
+    public static Table<T> MutableTableToTable<T>(this Mutable.Table<T> mutableTable) {
+      var head = mutableTable.Head;
+      var rows = mutableTable.Rows;
+      return Table<T>.Build(
+        head.ToArray(),
+        rows.Map(row => row.ToArray()).ToArray().NestToMatrix()
+      );
+    }
+  }
+}
