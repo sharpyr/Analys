7c0315a0e005a42c0f420461a217788502c4b93d
hoyeungw <hoyeungw@outlook.com>
2021-09-02 12:29:54 +0800

    add tests

---
+++ Analys.Test/Src/Mutable/PantoneOverlapTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Spare;
+using Veho;
+using Veho.List;
+using MU = Analys.Mutable;
+using Analys.Mutable.LinearAlgebra;
+
+namespace Analys.Test.Mutable {
+  [TestFixture]
+  public class PantoneOverlapTests {
+    [Test]
+    public void TestAlpha() {
+      var rows = Seq.From(
+        Seq.From(" ", " ", " ", " ", " ", " ", " ", "+", " ", " "),
+        Seq.From(" ", " ", "+", " ", "+", " ", " ", " ", " ", " "),
+        Seq.From(" ", "+", " ", " ", "+", " ", " ", " ", " ", " "),
+        Seq.From(" ", " ", " ", " ", " ", " ", "+", " ", "+", " "),
+        Seq.From(" ", "+", "+", " ", " ", " ", " ", " ", " ", " "),
+        Seq.From(" ", " ", " ", " ", " ", " ", " ", " ", " ", " "),
+        Seq.From(" ", " ", " ", "+", " ", " ", " ", " ", "+", " "),
+        Seq.From("+", " ", " ", " ", " ", " ", " ", " ", " ", " "),
+        Seq.From(" ", " ", " ", "+", " ", " ", "+", " ", " ", " "),
+        Seq.From(" ", " ", " ", " ", " ", " ", " ", " ", " ", " ")
+      );
+      var labels = Seq.From(
+        "S.Orange",
+        "Peach.C",
+        "C.Blush",
+        "Turmeric",
+        "Apricot.S",
+        "Chamois",
+        "B.M.gold",
+        "Amber.Y",
+        "Zinnia",
+        "Sahara"
+      );
+      var crostab = MU.Crostab<string>.Build(labels.Map(x => x), labels.Map(x => x), rows);
+      crostab.ToCrostab().Deco().Says("crostab");
+      var index = 0;
+      foreach (var diagonalBlock in crostab.IntersectionalBlocks("+")) {
+        diagonalBlock.ToCrostab().Deco().Says("diagonal");
+      }
+    }
+  }
+}
--- Analys.Test/Src/Strategies/CrostabStrategies.cs
+++ Analys.Test/Src/Strategies/CrostabStrategies.cs
@@ -1,56 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Analys.Mutable;
-using Analys.Test.Assets;
-using Analys.Types;
-using NUnit.Framework;
-using Spare;
-using Texting.Enums;
-using Typen;
-using Veho;
-using Veho.Enumerable;
-using Veho.Mutable.Matrix;
-using Veho.Types;
-using static System.Double;
-using static Palett.Presets;
-using Mu = Analys.Mutable;
-
-namespace Analys.Test.Strategies {
-  [TestFixture]
-  public class CrostabStrategies {
-    [Test]
-    public void CrostabBenchmarkSimple() {
-      var (elapsed, result) = Valjoux.Strategies.Run(
-        (int)2E+4,
-        Seq.From<(string, Func<Pivoted, Crostab<double>>)>(
-          ("DutyRoster", mode => TableCollection.DutyRosterTable.Crostab(("name", "day", "served"), mode)),
-          ("Borat", mode => TableCollection.BoratTable.Crostab(("name", "day", "kpi"), mode))
-        ),
-        Enum<Pivoted>.Values.Map((Pivoted pivoted) => (pivoted.Label(), pivoted)).ToList()
-      );
-      elapsed.Deco(orient: Operated.Rowwise, presets: (Subtle, Fresh)).Says("Elapsed", Strings.LF);
-      result.Deco().Says("Result", Strings.LF);
-      foreach (var name in Enum<Pivoted>.Names) {
-        result[name, "Borat"].Map(x => x.ToString("F2")).Deco(presets: (Subtle, Fresh)).Says("Borat : " + name, Strings.LF);
-      }
-    }
-    [Test]
-    public void MutableCrostabBenchmarkSimple() {
-      var fields = ("name", "day", "sold");
-      var (elapsed, result) = Valjoux.Strategies.Run(
-        (int)2E+4,
-        Seq.From<(string, Func<Pivoted, Mu::Crostab<double>>)>(
-          ("DutyRoster", mode => MutableTableCollection.DutyRosterTable.Crostab(("name", "day", "served"), mode)),
-          ("Borat", mode => MutableTableCollection.BoratTable.Crostab(("name", "day", "kpi"), mode))
-        ),
-        Enum<Pivoted>.Values.Map((Pivoted pivoted) => (pivoted.Label(), pivoted)).ToList()
-      );
-      elapsed.Deco(orient: Operated.Rowwise, presets: (Subtle, Fresh)).Says("Elapsed", Strings.LF);
-      result.Deco().Says("Result", Strings.LF);
-      foreach (var name in Enum<Pivoted>.Names) {
-        result[name, "Borat"].Map(x => x.ToString("F2")).ToCrostab().Deco(presets: (Subtle, Fresh)).Says("Borat : " + name, Strings.LF);
-      }
-    }
-  }
-}
+﻿// using System;
+// using System.Linq;
+// using Analys.Mutable;
+// using Analys.Test.Assets;
+// using Analys.Types;
+// using NUnit.Framework;
+// using Spare;
+// using Texting.Enums;
+// using Typen;
+// using Veho;
+// using Veho.Enumerable;
+// using Veho.Types;
+// using static Palett.Presets;
+// using Mu = Analys.Mutable;
+//
+// namespace Analys.Test.Strategies {
+//   [TestFixture]
+//   public class CrostabStrategies {
+//     [Test]
+//     public void CrostabBenchmarkSimple() {
+//       var (elapsed, result) = Valjoux.Strategies.Run(
+//         (int)2E+4,
+//         Seq.From<(string, Func<Pivoted, Crostab<double>>)>(
+//           ("DutyRoster", mode => TableCollection.DutyRosterTable.Crostab(("name", "day", "served"), mode)),
+//           ("Borat", mode => TableCollection.BoratTable.Crostab(("name", "day", "kpi"), mode))
+//         ),
+//         Enum<Pivoted>.Values.Map((Pivoted pivoted) => (pivoted.Label(), pivoted)).ToList()
+//       );
+//       elapsed.Deco(orient: Operated.Rowwise, presets: (Subtle, Fresh)).Says("Elapsed", Strings.LF);
+//       result.Deco().Says("Result", Strings.LF);
+//       foreach (var name in Enum<Pivoted>.Names) {
+//         result[name, "Borat"].Map(x => x.ToString("F2")).Deco(presets: (Subtle, Fresh)).Says("Borat : " + name, Strings.LF);
+//       }
+//     }
+//     [Test]
+//     public void MutableCrostabBenchmarkSimple() {
+//       var fields = ("name", "day", "sold");
+//       var (elapsed, result) = Valjoux.Strategies.Run(
+//         (int)2E+4,
+//         Seq.From<(string, Func<Pivoted, Mu::Crostab<double>>)>(
+//           ("DutyRoster", mode => MutableTableCollection.DutyRosterTable.Crostab(("name", "day", "served"), mode)),
+//           ("Borat", mode => MutableTableCollection.BoratTable.Crostab(("name", "day", "kpi"), mode))
+//         ),
+//         Enum<Pivoted>.Values.Map((Pivoted pivoted) => (pivoted.Label(), pivoted)).ToList()
+//       );
+//       elapsed.Deco(orient: Operated.Rowwise, presets: (Subtle, Fresh)).Says("Elapsed", Strings.LF);
+//       result.Deco().Says("Result", Strings.LF);
+//       foreach (var name in Enum<Pivoted>.Names) {
+//         result[name, "Borat"].Map(x => x.ToString("F2")).ToCrostab().Deco(presets: (Subtle, Fresh)).Says("Borat : " + name, Strings.LF);
+//       }
+//     }
+//   }
+// }
