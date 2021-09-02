ad4cb610d3b833f115cce6a1f7a86b6ed134cb8d
hoyeungw <hoyeungw@outlook.com>
2021-09-02 18:15:50 +0800

    add upper and lower triangular

--- Analys.Mutable/Src/LinearAlgebra/SymmetricMatrix.cs
+++ Analys.Mutable/Src/LinearAlgebra/SymmetricMatrix.cs
@@ -8,11 +8,26 @@ using Mu = Veho.Mutable.LinearAlgebra;
 namespace Analys.Mutable.LinearAlgebra {
   public static class SymmetricMatrix {
     public static Crostab<T> SelectBy<T>(Crostab<T> crostab, IReadOnlyList<int> indices) {
-      var labels = crostab.Side.SelectBy(indices);
-      var rows = Mu::SymmetricMatrix.SelectBy(crostab.Rows, indices);
-      return Crostab<T>.Build(labels.Slice(), labels.Slice(), rows);
+      return Crostab<T>.Build(
+        crostab.Side.SelectBy(indices),
+        crostab.Head.SelectBy(indices),
+        Mu::SymmetricMatrix.SelectBy(crostab.Rows, indices)
+      );
     }
-
+    public static Crostab<T> UpperTriangular<T>(Crostab<T> crostab) {
+      return Crostab<T>.Build(
+        crostab.Side.Slice(),
+        crostab.Head.Slice(),
+        Mu::SymmetricMatrix.UpperTriangular(crostab.Rows)
+      );
+    }
+    public static Crostab<T> LowerTriangular<T>(Crostab<T> crostab) {
+      return Crostab<T>.Build(
+        crostab.Side.Slice(),
+        crostab.Head.Slice(),
+        Mu::SymmetricMatrix.LowerTriangular(crostab.Rows)
+      );
+    }
     public static IEnumerable<Crostab<T>> IntersectionalBlocks<T>(this Crostab<T> crostab, T signal) where T : IEquatable<T> {
       var rowSkipper = Skipper<List<T>>.Build(crostab.Rows);
       foreach (var index in rowSkipper.IntoIndexIter()) {
