f7ff7932a877264bea186c8b1f39469d78753e64
hoyeungw <hoyeungw@outlook.com>
2021-09-03 13:53:20 +0800

    add tests

---
+++ Analys.Test/Src/PantoneOverlapTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Spare;
+using Veho;
+using Veho.Vector;
+using MU = Analys.Mutable;
+
+namespace Analys.Test {
+  [TestFixture]
+  public class PantoneOverlapTests {
+    [Test]
+    public void TestAlpha() {
+      var rows = new[,] {
+        { " ", " ", " ", " ", " ", " ", " ", "+", " ", " " },
+        { " ", " ", "+", " ", "+", " ", " ", " ", " ", " " },
+        { " ", "+", " ", " ", "+", " ", " ", " ", " ", " " },
+        { " ", " ", " ", " ", " ", " ", "+", " ", "+", " " },
+        { " ", "+", "+", " ", " ", " ", " ", " ", " ", " " },
+        { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+        { " ", " ", " ", "+", " ", " ", " ", " ", "+", " " },
+        { "+", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+        { " ", " ", " ", "+", " ", " ", "+", " ", " ", " " },
+        { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " }
+      };
+      var labels = Vec.From(
+        "S.Orange",
+        "Peach.C",
+        "C.Blush",
+        "Turmeric",
+        "Apricot.S",
+        "Chamois",
+        "B.M.gold",
+        "Amber.Y",
+        "Zinnia",
+        "Sahara"
+      );
+      var crostab = Crostab<string>.Build(labels.Slice(), labels.Slice(), rows);
+      crostab.Deco().Says("crostab");
+      foreach (var diagonalBlock in crostab.IntersectionalBlocks("+")) {
+        diagonalBlock.Deco().Says("diagonal");
+      }
+      crostab.SelectBy(Vec.From("Peach.C", "C.Blush", "Apricot.S", "Amber.Y"), Vec.From("Peach.C", "C.Blush", "Amber.Y")).Deco().Says("selected");
+    }
+  }
+}
