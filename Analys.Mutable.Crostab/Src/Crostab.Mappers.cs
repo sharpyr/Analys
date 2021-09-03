bf9886355af91c584ea220a11d856cfd19f9fd78
hoyeungw <hoyeungw@outlook.com>
2021-09-03 13:53:01 +0800

    add map functions for Crostab<T>

--- Analys.Mutable.Crostab/Src/Crostab.Mappers.cs
+++ Analys.Mutable.Crostab/Src/Crostab.Mappers.cs
@@ -6,11 +6,14 @@ using Veho.Sequence;
 
 namespace Analys.Mutable {
   public partial class Crostab<T> {
-    public Crostab<TO> Map<TO>(Func<T, TO> fn) => Crostab<TO>.Build(
-      Side.Slice(),
-      Head.Slice(),
-      Rows.Map(fn)
-    );
+    public Crostab<TO> Map<TO>(Func<T, TO> fn) => Crostab<TO>.Build(Side.Slice(), Head.Slice(), Rows.Map(fn));
+    public Crostab<TO> Map<TO>(Func<int, int, T, TO> fn) => Crostab<TO>.Build(Side.Slice(), Head.Slice(), Rows.Map(fn));
+    public void Iterate(Action<string, string, T> action) {
+      this.Rows.Iterate((i, j, cell) => action(this.Side[i], this.Head[j], cell));
+    }
+    public void Iterate(Action<int, int, string, string, T> action) {
+      this.Rows.Iterate((i, j, cell) => action(i, j, this.Side[i], this.Head[j], cell));
+    }
     public IEnumerable<(string, List<T>)> RowsIter() {
       for (int i = 0, hi = this.Height; i < hi; i++) yield return (this.Side[i], this.Rows[i]);
     }
