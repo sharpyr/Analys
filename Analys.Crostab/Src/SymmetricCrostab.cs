6fdca22e9676853daa374d988dda78ef2596af74
hoyeungw <hoyeungw@outlook.com>
2021-09-03 13:52:45 +0800

    add SymmetricCrostab for immutable crostab

---
+++ Analys.Crostab/Src/SymmetricCrostab.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Veho.Matrix;
+using Veho.Vector;
+
+
+namespace Analys {
+  public static class SymmetricCrostab {
+    public static Crostab<T> SelectBy<T>(Crostab<T> crostab, IReadOnlyList<int> indices) {
+      return Crostab<T>.Build(
+        crostab.Side.SelectBy(indices),
+        crostab.Head.SelectBy(indices),
+        SymmetricMatrix.SelectBy(crostab.Rows, indices)
+      );
+    }
+    public static Crostab<T> SelectBy<T>(Crostab<T> crostab, IReadOnlyList<string> labels) {
+      var indices = labels.Map(x => crostab.Side.IndexOf(x));
+      return SymmetricCrostab.SelectBy(crostab, indices);
+    }
+    public static Crostab<T> UpperTriangular<T>(Crostab<T> crostab) {
+      return Crostab<T>.Build(
+        crostab.Side.Slice(),
+        crostab.Head.Slice(),
+        SymmetricMatrix.UpperTriangular(crostab.Rows)
+      );
+    }
+    public static Crostab<T> LowerTriangular<T>(Crostab<T> crostab) {
+      return Crostab<T>.Build(
+        crostab.Side.Slice(),
+        crostab.Head.Slice(),
+        SymmetricMatrix.LowerTriangular(crostab.Rows)
+      );
+    }
+    public static IEnumerable<Crostab<T>> IntersectionalBlocks<T>(this Crostab<T> crostab, T signal) where T : IEquatable<T> {
+      return crostab.Rows
+                    .IntersectionalIndices(signal)
+                    .Select(intersectionalIndices => SymmetricCrostab.SelectBy(crostab, intersectionalIndices.ToList()));
+    }
+  }
+}
