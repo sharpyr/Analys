17832a0b3b7433779c8ca15c8813fd688e6dd66f
hoyeungw <hoyeungw@outlook.com>
2021-09-27 10:52:31 +0800

    add selectors for Table<T>

---
+++ Analys.Table/Src/Table.Selector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Veho.Rows;
+using Veho.Vector;
+
+namespace Analys {
+  public partial class Table<T> {
+    public T[,] SelectBy(IReadOnlyList<string> headLabels) {
+      var indices = headLabels.Map(this.CoIn);
+      return this.Rows.SelectRows(indices);
+    }
+    public T[,] SelectOf(params string[] headLabels) {
+      return this.SelectBy(headLabels);
+    }
+    public IEnumerable<T[]> IntoRowsIter(params string[] headLabels) {
+      var indices = headLabels.Map(this.CoIn);
+      return this.Rows.IntoRowsIter(indices);
+    }
+    public IEnumerable<T[]> IntoRowsIter(params Regex[] headLabels) {
+      var indices = headLabels.Map(this.CoIn);
+      return this.Rows.IntoRowsIter(indices);
+    }
+  }
+}
