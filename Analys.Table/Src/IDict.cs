14cbaff5168c5196e6a104755952d3d131bd07ad
hoyeungw <hoyeungw@outlook.com>
2021-10-07 20:52:35 +0800

    add interface IDict

---
+++ Analys.Table/Src/IDict.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Analys {
+  public interface IDict<TKey, TValue> : IEnumerable<(TKey key, TValue value )> {
+    IReadOnlyList<TKey> Keys { get; }
+    IReadOnlyList<TValue> Values { get; }
+    int Count { get; }
+    TValue this[TKey key] { get; }
+    bool Has(string key);
+    bool TryLookup(TKey key, out TValue value);
+  }
+}
