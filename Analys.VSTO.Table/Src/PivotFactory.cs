f079579e4bc4c67d0c12c52b84ff93ec3061bd4f
hoyeungw <hoyeungw@outlook.com>
2021-10-29 11:02:35 +0800

    add PivotFactory for ExcelTable

---
+++ Analys.VSTO.Table/Src/PivotFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Analys.Stat;
+using Analys.Types;
+using Acc = Analys.Pivot.Struct.Accumulators;
+using Init = Analys.Pivot.Struct.Initializers;
+
+namespace Analys.VSTO {
+  public static class PivotFactory {
+    public static Pivot<TAccum, TSample> Build<TAccum, TSample>(
+      Func<TAccum> initializer,
+      Func<TAccum, TSample, TAccum> accumulator
+    ) => new Pivot<TAccum, TSample> {
+                                      Accumulator = accumulator,
+                                      Init = initializer,
+                                      Side = new string[] { },
+                                      Head = new string[] { },
+                                      Rows = new TAccum[][] { },
+                                    };
+
+    public static Pivot<IStat, double> Build(
+      Pivoted pivoted
+    ) => new Pivot<IStat, double> {
+                                    Accumulator = StatExt.Record,
+                                    Init = GadgetFactory.Build(pivoted),
+                                    Side = new string[] { },
+                                    Head = new string[] { },
+                                    Rows = new IStat[][] { },
+                                  };
+
+    public static Pivot<int, double> Count() => Build<int, double>(Init.Count, Acc.Count);
+    public static Pivot<double, double> Sum() => Build<double, double>(Init.Sum, Acc.Sum);
+  }
+}
