d77f54c2be614a45ac258c9d6469b08e5cfc5661
hoyeungw <hoyeungw@outlook.com>
2021-10-29 11:00:48 +0800

    add more function to TableToCrostab

--- Analys.Convert/Src/TableToCrostab.cs
+++ Analys.Convert/Src/TableToCrostab.cs
@@ -4,9 +4,8 @@ using Typen;
 
 namespace Analys {
   public static class TableToCrostab {
-
     public static Crostab<double> Crostab(
-      this Table<object> table,
+      this Table table,
       (string side, string head, string field) fields,
       Pivoted mode,
       Func<object, double> parser = null
@@ -15,8 +14,23 @@ namespace Analys {
       var indexes = table.GetIndexes(fields);
       switch (mode) {
         case Pivoted.Count: return PivotFactory.Count(indexes).Record(table.Rows, parser).ToCrostab(x => (double)x);
-        case Pivoted.Sum: return PivotFactory.Sum(indexes).Record(table.Rows, parser).ToCrostab();
-        default: return PivotFactory.Build(indexes, mode).Record(table.Rows, parser).ToCrostab(x => x.Value);
+        case Pivoted.Sum:   return PivotFactory.Sum(indexes).Record(table.Rows, parser).ToCrostab();
+        default:            return PivotFactory.Build(indexes, mode).Record(table.Rows, parser).ToCrostab(x => x.Value);
+      }
+    }
+
+    public static Crostab<double> Crostab<T>(
+      this Table<T> table,
+      (string side, string head, string field) fields,
+      Pivoted mode,
+      Func<T, double> parser
+    ) {
+      // if (parser == null) parser = Numeral.CastDouble;
+      var indexes = table.GetIndexes(fields);
+      switch (mode) {
+        case Pivoted.Count: return PivotFactory.Count(indexes).Record(table.Rows, parser).ToCrostab(x => (double)x);
+        case Pivoted.Sum:   return PivotFactory.Sum(indexes).Record(table.Rows, parser).ToCrostab();
+        default:            return PivotFactory.Build(indexes, mode).Record(table.Rows, parser).ToCrostab(x => x.Value);
       }
     }
   }
