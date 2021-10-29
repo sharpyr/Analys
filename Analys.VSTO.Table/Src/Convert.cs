f7fe7343c8d8f07976883eddd749d7a793b6437a
hoyeungw <hoyeungw@outlook.com>
2021-10-29 11:02:05 +0800

    add convert function ToAlgebraicTable

---
+++ Analys.VSTO.Table/Src/Convert.cs
@@ -0,0 +1,7 @@
+﻿namespace Analys.VSTO {
+  public static class Converters {
+    public static AlgebraicTable<T> ToAlgebraicTable<T>(this Table<T> table) {
+      return new AlgebraicTable<T> { Head = table.Head, Rows = table.Rows };
+    }
+  }
+}
