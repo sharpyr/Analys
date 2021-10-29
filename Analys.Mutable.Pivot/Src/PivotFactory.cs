b6cf4efb1597cf246f4ab2f769cabba9f0122344
hoyeungw <hoyeungw@outlook.com>
2021-10-29 11:01:22 +0800

    simplified code for PivotFactory

--- Analys.Mutable.Pivot/Src/PivotFactory.cs
+++ Analys.Mutable.Pivot/Src/PivotFactory.cs
@@ -12,28 +12,26 @@ namespace Analys.Mutable {
       FieldIndexes indexes,
       Func<TAccum> initializer,
       Func<TAccum, TSample, TAccum> accumulator
-    ) =>
-      new Pivot<TAccum, TSample> {
-        Indexes = indexes,
-        Accumulator = accumulator,
-        Init = initializer,
-        Side = new List<string> { },
-        Head = new List<string> { },
-        Rows = new List<List<TAccum>> { },
-      };
+    ) => new Pivot<TAccum, TSample> {
+                                      Indexes = indexes,
+                                      Accumulator = accumulator,
+                                      Init = initializer,
+                                      Side = new List<string>(),
+                                      Head = new List<string>(),
+                                      Rows = new List<List<TAccum>>(),
+                                    };
 
     public static Pivot<IStat, double> Build(
       FieldIndexes indexes,
       Pivoted pivoted
-    ) =>
-      new Pivot<IStat, double> {
-        Indexes = indexes,
-        Accumulator = StatExt.Record,
-        Init = GadgetFactory.Build(pivoted),
-        Side = new List<string> { },
-        Head = new List<string> { },
-        Rows = new List<List<IStat>> { },
-      };
+    ) => new Pivot<IStat, double> {
+                                    Indexes = indexes,
+                                    Accumulator = StatExt.Record,
+                                    Init = GadgetFactory.Build(pivoted),
+                                    Side = new List<string>(),
+                                    Head = new List<string>(),
+                                    Rows = new List<List<IStat>>(),
+                                  };
 
     public static Pivot<int, double> Count(FieldIndexes indexes) => PivotFactory.Build<int, double>(indexes, Init.Count, Acc.Count);
     public static Pivot<double, double> Sum(FieldIndexes indexes) => PivotFactory.Build<double, double>(indexes, Init.Sum, Acc.Sum);
