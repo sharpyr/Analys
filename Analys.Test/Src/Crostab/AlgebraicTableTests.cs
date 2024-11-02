cea0af2093e843d599b29146884d28eaac81c137
hoyeungw <hoyeungw@outlook.com>
2024-11-02 11:25:15 +0800

    trim unnecessary projects

--- Analys.Test/Src/Crostab/AlgebraicTableTests.cs
+++ Analys.Test/Src/Crostab/AlgebraicTableTests.cs
@@ -1,26 +1,26 @@
-﻿using Analys.Test.Assets;
-using Analys.Types;
-using Analys.VSTO;
-using NUnit.Framework;
-using Spare;
-using Veho;
-
-namespace Analys.Test.Crostab {
-  [TestFixture]
-  public class AlgebraicTableTests {
-    [Test]
-    public void AlgebraicExcelTableTestAlpha() {
-      var tableSpecs = Seq.From(
-        ("date", "sid", "rev[a] / sold[a]", Pivoted.Sum),
-        ("date", "sid", "rev[a]", Pivoted.Sum),
-        ("date", "sid", "rev[a] / sold[a] * 100", Pivoted.Sum)
-        // ("date", "sid", "rev[a] / no_exist * 100", Pivoted.Sum)
-      );
-      var table = TableCollection.FieldPerformanceTable.ToAlgebraicTable();
-      foreach (var (side, head, formula, mode) in tableSpecs) {
-        var crostab = table.AlgebraicCrostab((side, head, formula), mode);
-        crostab.Deco().Says($"{mode}({formula}) by {side} and {head}");
-      }
-    }
-  }
-}
+﻿// using Analys.Test.Assets;
+// using Analys.Types;
+// // using Analys.VSTO;
+// using NUnit.Framework;
+// using Spare;
+// using Veho;
+//
+// namespace Analys.Test.Crostab {
+//   [TestFixture]
+//   public class AlgebraicTableTests {
+//     [Test]
+//     public void AlgebraicExcelTableTestAlpha() {
+//       var tableSpecs = Seq.From(
+//         ("date", "sid", "rev[a] / sold[a]", Pivoted.Sum),
+//         ("date", "sid", "rev[a]", Pivoted.Sum),
+//         ("date", "sid", "rev[a] / sold[a] * 100", Pivoted.Sum)
+//         // ("date", "sid", "rev[a] / no_exist * 100", Pivoted.Sum)
+//       );
+//       var table = TableCollection.FieldPerformanceTable.ToAlgebraicTable();
+//       foreach (var (side, head, formula, mode) in tableSpecs) {
+//         var crostab = table.AlgebraicCrostab((side, head, formula), mode);
+//         crostab.Deco().Says($"{mode}({formula}) by {side} and {head}");
+//       }
+//     }
+//   }
+// }
